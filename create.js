1ad1177845dca29f00a3964e5f029e91e71f2d7b
Asif ADAMSHA <a.adamsha@magneta.fr>
2020-12-12 23:23:31 +0100

    Create endpoint working

---
+++ create.js
@@ -0,0 +1,34 @@
+import * as uuid from "uuid";
+import AWS from "aws-sdk";
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+export async function main(event, context) {
+
+    const data = JSON.parse(event.body);
+
+    const params = {
+        TableName: process.env.tableName,
+        Item: {
+            userId: "123", // The id of the author
+            noteId: uuid.v1(), // A unique uuid
+            content: data.content, // Parsed from request body
+            attachment: data.attachment, // Parsed from request body
+            createdAt: Date.now(), // Current Unix timestamp
+          },
+    };
+
+    try {
+        await dynamoDb.put(params).promise();
+        return {
+          statusCode: 200,
+          body: JSON.stringify(params.Item),
+        };
+    } catch (e) {
+        return {
+          statusCode: 500,
+          body: JSON.stringify({ error: e.message }),
+        };
+    }
+
+}
